5ab5ded549258dac505a0c0b0297297890007fe6
Rogelio Servin <manchaster1509@gmail.com>
2024-02-12 21:21:50 -0600

    Nav

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@ import { createClient } from "@/utils/supabase/server";
 
 export default async function Index() {
   return (
-    <div className="flex flex-col items-center flex-1 w-full gap-20"></div>
+    <div className="flex flex-col items-center flex-1 w-full gap-20">
+      <nav className="flex justify-center w-full h-16 border-b border-b-foreground/10">
+        <div className="flex items-center justify-between w-full max-w-4xl p-3 text-sm">
+          <DeployButton />
+          {isSupabaseConnected && <AuthButton />}
+        </div>
+      </nav>
+    </div>
   );
 }
