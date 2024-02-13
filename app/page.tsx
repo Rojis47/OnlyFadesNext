3381a697b6c1c4daf336f1f3068bd18405e1fc78
Rogelio Servin <manchaster1509@gmail.com>
2024-02-12 20:57:35 -0600

    firs commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Index() {
+  return (
+    <div className="flex flex-col items-center flex-1 w-full gap-20"></div>
+  );
+}
