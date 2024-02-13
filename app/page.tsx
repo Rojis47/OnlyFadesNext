8834c22784a08ad70b197b210d8be54b74bfd496
Julio Aguilera Jr <julioaguilera337@gmail.com>
2024-02-12 21:28:47 -0600

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
+import AuthButton from "@/components/AuthButton";
+import DeployButton from "@/components/DeployButton";
 import { createClient } from "@/utils/supabase/server";
 
 export default async function Index() {
+  const canInitSupabaseClient = () => {
+    // This function is just for the interactive tutorial.
+    // Feel free to remove it once you have Supabase connected.
+    try {
+      createClient();
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  const isSupabaseConnected = canInitSupabaseClient();
   return (
     <div className="flex flex-col items-center flex-1 w-full gap-20">
       <nav className="flex justify-center w-full h-16 border-b border-b-foreground/10">
