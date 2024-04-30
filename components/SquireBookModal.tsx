d9c9427ef04c65c0b936a92df371fb545e919bfc
Rogelio Servin <manchaster1509@gmail.com>
2024-04-29 21:54:00 -0500

    update book modal

--- components/SquireBookModal.tsx
+++ components/SquireBookModal.tsx
@@ -1,3 +1,6 @@
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 import { TBarber, BarberSchema } from "../app/types";
 import { Spinner } from "@nextui-org/react";
 
@@ -19,52 +22,77 @@ function SquireBookModal({
   const result = BarberSchema.parse(barber);
 
   return (
-    <div>
-      {showModal ? (
-        <div className={`relative z-30 block top-0`}>
-          <div className="fixed inset-0 transition-opacity bg-black bg-opacity-75"></div>
-          <div className="fixed inset-0 overflow-y-auto lg:max-w-96 lg:m-auto">
-            <div className="sticky top-0 pt-4 text-white text-end">
-              <button
-                title="modal"
-                className="z-10"
-                onClick={() => setShowModal(false)}
+    <Transition.Root show={showModal} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setShowModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-in-out duration-500"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in-out duration-500"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 transition-opacity backdrop-blur-md bg-opacity-10" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="fixed inset-y-0 right-0 flex max-w-full pl-10 pointer-events-none">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-10 h-10 "
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </button>
+                <Dialog.Panel className="w-screen max-w-md pointer-events-auto">
+                  <div className="flex flex-col h-full overflow-y-scroll bg-white shadow-xl">
+                    <div className="flex-1 px-4 py-6 overflow-y-auto sm:px-6">
+                      <div className="flex items-start justify-between">
+                        <div className="flex items-center ml-3 h-7">
+                          <button
+                            type="button"
+                            className="relative p-2 -m-2 text-gray-400 hover:text-gray-500"
+                            onClick={() => setShowModal(false)}
+                          >
+                            <span className="absolute -inset-0.5" />
+                            <span className="sr-only">Close panel</span>
+
+                            <XMarkIcon
+                              className="w-10 h-10 text-black"
+                              aria-hidden="true"
+                            />
+                          </button>
+                        </div>
+                      </div>
+
+                      {isLoading && (
+                        <div className="absolute z-0 translate-y-1/2 right-1/2 bottom-1/2 ">
+                          <Spinner className="z-0" />
+                        </div>
+                      )}
+                      <iframe
+                        className="w-full h-full rounded-xl"
+                        src={`https://getsquire.com/booking/book/${
+                          barber.location
+                        }/${barber.role.toLowerCase()}/${
+                          barber.squireId
+                        }/services?platform=widget`}
+                        title="External Content"
+                        onLoad={() => setIsLoading(false)}
+                      />
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
             </div>
-            {isLoading && (
-              <div className="absolute z-0 translate-y-1/2 right-1/2 bottom-1/2 ">
-                <Spinner className="z-0" />
-              </div>
-            )}
-            <iframe
-              className="w-full rounded-xl h-4/5 lg:h-5/6 lg:max-w-96 lg:m-auto"
-              src={`https://getsquire.com/booking/book/${
-                barber.location
-              }/${barber.role.toLowerCase()}/${
-                barber.squireId
-              }/services?platform=widget`}
-              title="External Content"
-              onLoad={() => setIsLoading(false)}
-            />
           </div>
         </div>
-      ) : null}
-    </div>
+      </Dialog>
+    </Transition.Root>
   );
 }
 
